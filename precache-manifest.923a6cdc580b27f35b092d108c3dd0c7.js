eec9aeb4efd57e66666f239a4da4822e46929746
Ananya Verma <lincolnav829@gmail.com>
2020-08-06 02:56:00 +0530

    Updates

---
+++ precache-manifest.923a6cdc580b27f35b092d108c3dd0c7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cb8b97204e41f55d1334cfc685007f7",
+    "url": "/github-card/index.html"
+  },
+  {
+    "revision": "7e5b3e71d45f7e8f2b3c",
+    "url": "/github-card/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5d424a02f0c50aec5de5",
+    "url": "/github-card/static/css/main.c23fd164.chunk.css"
+  },
+  {
+    "revision": "7e5b3e71d45f7e8f2b3c",
+    "url": "/github-card/static/js/2.f43cc0dc.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/github-card/static/js/2.f43cc0dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d424a02f0c50aec5de5",
+    "url": "/github-card/static/js/main.87259dfb.chunk.js"
+  },
+  {
+    "revision": "0515e9d2d4317112365c",
+    "url": "/github-card/static/js/runtime-main.61439255.js"
+  },
+  {
+    "revision": "9418b8d8c6f7959b83bb36ecac604252",
+    "url": "/github-card/static/media/anu.9418b8d8.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-card/static/media/logo.5d5d9eef.svg"
+  }
+]);
